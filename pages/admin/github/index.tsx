a1a719e6951ae093c9d209ccc922b347a4d857be
ViLH <vi.lh@>
2022-10-31 10:40:55 +0700

    add SWR, wrong destructering variable so dont show data

--- pages/admin/github/index.tsx
+++ pages/admin/github/index.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 
+import fetch from 'unfetch'
 import React, { useState, useEffect } from "react";
+
 
 // layout for page
 
@@ -12,55 +14,8 @@ import useSWR, { SWRConfig } from "swr"
 
 // https://swr.vercel.app/examples/ssr SSR SWR
 
-const API = `https://api.github.com/users`
-
-// use SWR
-const fetcher = async(url: String) => {
-    const rel = await axios.get(url)
-    return rel.data
-
-    // const fetcher = (...args) => fetch(...args).then((res) => res.data)
-}
-
-
-export async function getServerSideProps() {
-    const repoInfo = await fetcher(API);
-    return {
-      props: {
-        fallback: {
-          [API]: repoInfo
-        }
-      }
-    };
-  }
-  
-
-   
-
-export default function Githubs( { fallback } ) {
-
-  const { language } = useTrans()
-
-   
-   // use axios
-//    const [ datas, setDatas ] = useState([])
-//    const fetchAPI = async() => {
-//     const rel = await axios.get(address)
-//     setDatas(rel.data)
-//    }   
-//    useEffect(() => {
-//     fetchAPI() 
-// }, [])
-
-
-   
-   const { users, error } = useSWR(API)
-   
-
-
 
   // https://api.github.com/users?since=49
-
 //   [
 //     {
 //     "login": "jwhitmire",
@@ -84,44 +39,91 @@ export default function Githubs( { fallback } ) {
 //     },
 //  ]
 
-  return ( 
-        <SWRConfig value={{ fallback }}>
-            <div className="flex flex-wrap mt-40 mb-4">
-                {  error ? "Loading failed " : (!users ? "Loading ..." : "") } 
-            </div> 
+// export async function getServerSideProps() {
+//     // Fetch data from external API
+//     const endpoint = `https://api.github.com/users`
+//     // const res = await fetch(endpoint)
+//     const res = await axios.get(endpoint)
+//     const fetcher = await res.data
 
-            <div className="flex flex-wrap mt-40 mb-4">
-                <h2 className="uppercase font-bold text-theme"> Github Users </h2>
-            </div>
+  
+//     // Pass data to the page via props
+//     return { props: { endpoint, fetcher } }
+// } 
 
-            <div className="flex flex-wrap mt-2 ">
-                <div className="w-full mb-12 px-4 border shadow rounded p-4">
-                    
-                    <div className="grid grid-cols-8 gap-4">          
-                        {
-                            users && users.map((value, index) => {
-                                return (
-                                    <div className="p-2 bg-white shadow-lg rounded " key={ value.id }> 
-                                    <div className="w-50 h-50">
-                                        <Image src={ value.avatar_url } width={'100%'} height={'100%'} alt={ value.login } /> 
-                                    </div>
-                                    <div> Login: { value.login }   </div>
-                                    <div>
-                                        Url: 
-                                        {/* <Link href={ value.url } > { value.url } </Link> */}
-                                    </div>
-                                    <div> Id: { value.id }   </div>
-                                    <div> Node: { value.node_id }   </div>
-                                </div>
-                                )
-                                
-                            })
-                        }
+  
+const fetcher = async() => {
+    const endpoint = `https://api.github.com/users`
+    const res = await axios.get(endpoint) 
+    // console.log(res)
+    return await res.data
+}
+ 
+export default function Githubs(   ) {
+ 
+  
+  const { language } = useTrans()
+  const { data,  isValidating  } = useSWR(`https://api.github.com/users`, fetcher)
+
+  console.log(data,  isValidating )
+
+ 
+// use axios
+//    const [ datas, setDatas ] = useState([])
+//    const fetchAPI = async() => {
+//     //const rel = await axios.get(endpoint)
+//     const rel = await fetch(endpoint, "GET")
+//     console.log(rel.data);
+//     // setDatas(rel.data)
+//    }   
+//    useEffect(() => {
+//     fetchAPI() 
+// }, [])
+    
+  return (  
+    <>
+        <div className="flex flex-wrap mt-40 mb-4">
+            {  isValidating ? "Loading failed " : (!data ? "Loading ..." : "") } 
+        </div> 
+
+        <div className="flex flex-wrap mt-40 mb-4">
+            <h2 className="uppercase font-bold text-theme"> Github Users </h2>
+        </div>
+
+        <div className="flex flex-wrap mt-2 ">
+            <div className="w-full mb-12 px-4 border shadow rounded p-4">
                 
-                    </div>
+                <div className="grid lg:grid-cols-8 gap-4 sm:grid-cols-4 xs:grid-cols-2">          
+                    {
+                        data && data.map((value, index) => {
+                            return (
+                                <div className="p-2 bg-white shadow-lg rounded " key={ value.id }> 
+                                <div className="w-50 h-50">
+                                    <Image src={ value.avatar_url } width={'100%'} height={'100%'} alt={ value.login } /> 
+                                </div>
+                                <div> Login: { value.login }   </div>
+                                <div>
+                                    Url: 
+                                    {/* <Link href={ value.url } > { value.url } </Link> */}
+                                </div>
+                                <div> Id: { value.id }   </div>
+                                <div>
+                                    Node:  <span className="text-xs font-bold"> { value.node_id }   </span>
+                                </div>
+                            </div>
+                            )
+                            
+                        })
+                    }
+            
+                </div>
+
+                <div className="mt-4 text-center" > 
+                    <button className=" rounded px-6 py-2 bg-theme text-white uppercase"> Load more ... </button>
                 </div>
             </div>
-        </SWRConfig> 
+        </div>  
+    </>
   );
 }
 
--- public/lang/zho.js
+++ public/lang/zho.js
@@ -1,7 +1,7 @@
 export default {
     home: {
-        title: '早上好',
-        content: '大家好呀， 歡迎來到 Learn tech Tips 頻道'
+        title: '早上好， 善為',
+        content: '歡迎來到 Learn tech Tips 頻道'
     },
     operation: "操作"
 }
