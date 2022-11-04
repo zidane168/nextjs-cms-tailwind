fb729108ee9f72bf4930154907f783ce5215645a
ViLH <vi.lh@>
2022-11-04 15:40:37 +0700

    load more

--- pages/admin/github2/index.tsx
+++ pages/admin/github2/index.tsx
@@ -21,10 +21,11 @@ import useSWRInfinite from "swr/infinite";
 export default function Github2() {
   const { language } = useTrans();  
 
+  const LIMIT: number = 10
+
   const fetcher = async (url: String) => {  
     const res = await axios.get(url);  
-    return res.data.params.list
-    
+     return res.data.params.list   
   };
 
   const onHandleClickAddMore = () => {
@@ -32,13 +33,16 @@ export default function Github2() {
   }
   
   const { data, error, mutate, size, setSize, isValidating } = useSWRInfinite(
-    (index) => `http://localhost:3333/githubs?limit=3&page=${index + 1}`,
+    (index) => `http://localhost:3333/githubs?limit=${LIMIT}&page=${index + 1}`,
     fetcher
-  );  
+  );   
  
-  const datas = data ? [].concat(...data) : []
+  const datas = data ? [].concat(...data) : []    
+  const isEmpty = data?.[0]?.length === 0;
+  const isReachingEnd = isEmpty || (data && data[data.length - 1]?.length < LIMIT);
+ 
   const isLoadingInitialData = !data
-  const isRefreshing = isValidating && data && data.length === size;
+  //const isRefreshing = isValidating && data && data.length === size;
 
   return (
     <>
@@ -57,7 +61,7 @@ export default function Github2() {
                 !!datas?.length  && datas?.map((value: string, index: number) => {
                 
                   return (
-                      <div className="p-2 bg-white shadow-lg rounded " key={ value.id }> 
+                    <div className="p-2 bg-white shadow-lg rounded " key={ value.id }> 
                       <div className="w-50 h-50">
                           <Image src={ "http://localhost:3333/" + value.githubImages[0].myFiles[0].path } width={'100%'} height={'100%'} alt={ value.login } /> 
                       </div>
@@ -76,15 +80,17 @@ export default function Github2() {
             }
           </div>
 
-          <div className="mt-4 text-center">
-            <button
-              className=" rounded px-6 py-2 bg-theme text-white uppercase"
-              onClick={ onHandleClickAddMore }
-            >
-              {" "}
-              Load more ...{" "}
-            </button>
-          </div>
+          { 
+            !isReachingEnd ?  
+              <div className="mt-4 text-center">
+                <button
+                  className=" rounded px-6 py-2 bg-theme text-white uppercase"
+                  onClick={ onHandleClickAddMore }
+                > 
+                  Load more ... 
+                </button>
+              </div>   : ' '
+          }
         </div>
       </div>
     </>
