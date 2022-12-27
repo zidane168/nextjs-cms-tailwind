5de8b0275c8e7a09c45d44f3328292776a133219
ViLH <vi.lh@>
2022-12-27 17:21:28 +0700

    sync

--- pages/admin/member/ui/sale.tsx
+++ pages/admin/member/ui/sale.tsx
@@ -102,7 +102,7 @@ export default function sale() {
                 Buy now
               </button>
               <button
-                className="h-10 px-6 font-semibold rounded-md border border-slate-200 text-slate-900"
+                className="h-10 px-6 font-semibold rounded-md border border-slate-200 text-slate-900 w-full"
                 type="button"
               >
                 Add to bag
